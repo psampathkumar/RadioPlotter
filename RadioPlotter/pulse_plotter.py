80e278d0cb5e57c3a6592a872469115c73457651
Jelena Petereit <108803123+jelenakhlr@users.noreply.github.com>
2024-04-04 16:40:37 +0200

    good code and other witchcraft

--- RadioPlotter/pulse_plotter.py
+++ RadioPlotter/pulse_plotter.py
@@ -2,13 +2,12 @@
 """
 Take antenna number as input and plot the pulses.
 """
-import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def pulse_plotter(antenna_number, pulses):
     """
-    Assume the pulse array is setup as 
+    Assume the pulse array is setup as
     3x256x240, for 3 polaritations, 256 time bins and 240 antennas
     """
     antenna_data = pulses[:, :, antenna_number]
@@ -18,13 +17,13 @@ def pulse_plotter(antenna_number, pulses):
     ezc7 = antenna_data[:, -1]
 
     plt.title(f"pulses for antenna {antenna_number}")
-    plt.plot(timec7, exc7, label = "x polarization")
-    plt.plot(timec7, eyc7, label = "y polarization")
-    plt.plot(timec7, ezc7, label = "z polarization")
+    plt.plot(timec7, exc7, label="x polarization")
+    plt.plot(timec7, eyc7, label="y polarization")
+    plt.plot(timec7, ezc7, label="z polarization")
     plt.xlabel("time in bins")
     plt.ylabel("efield")
     plt.legend()
-    plt.savefig(f"pulses_antenna_{antenna_number}.png", dpi = 300)
+    plt.savefig(f"pulses_antenna_{antenna_number}.png", dpi=300)
     plt.close()
 
     return None
