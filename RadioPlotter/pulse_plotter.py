6d6f2e30491d45e68dc4f2f39ce550fafdfc4aa3
Jelena Petereit <108803123+jelenakhlr@users.noreply.github.com>
2024-04-04 16:22:31 +0200

    change to using pulse array as input

--- RadioPlotter/pulse_plotter.py
+++ RadioPlotter/pulse_plotter.py
@@ -6,20 +6,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def pulse_plotter(antenna_number, file):
+def pulse_plotter(antenna_number, pulses):
     """
-    Assume file is setup as 
-    x y z time antenna_number
-    and each row corresponds to one antenna number
+    Assume the pulse array is setup as 
+    3x256x240, for 3 polaritations, 256 time bins and 240 antennas
     """
-    names = ["pulse_x", "pulse_y", "pulse_z", "time", "antennaID"]
-    pulses = pd.read_csv(file, names=names)
-    pulses = pulses[pulses.antennaID == antenna_number]
+    antenna_data = pulses[:, :, antenna_number]
+    timec7 = antenna_data[:, -4]
+    exc7 = antenna_data[:, -3]
+    eyc7 = antenna_data[:, -2]
+    ezc7 = antenna_data[:, -1]
 
     plt.title(f"pulses for antenna {antenna_number}")
-    plt.plot(pulses["pulse_x"], label = "x polarization")
-    plt.plot(pulses["pulse_y"], label = "y polarization")
-    plt.plot(pulses["pulse_z"], label = "z polarization")
+    plt.plot(timec7, exc7, label = "x polarization")
+    plt.plot(timec7, eyc7, label = "y polarization")
+    plt.plot(timec7, ezc7, label = "z polarization")
+    plt.xlabel("time in bins")
+    plt.ylabel("efield")
+    plt.legend()
     plt.savefig(f"pulses_antenna_{antenna_number}.png", dpi = 300)
     plt.close()
 
