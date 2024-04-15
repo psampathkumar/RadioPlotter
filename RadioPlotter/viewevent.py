f49c85d0248686cb9b3c4e345c773d2de9f3730f
Pranav <pranav.sampathkumar@gmail.com>
2024-04-15 19:14:46 +0200

    Fix being able to add mutliple datasets
    use special radiobuttons from stackoverflow

--- RadioPlotter/viewevent.py
+++ RadioPlotter/viewevent.py
@@ -1,7 +1,92 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.interpolate as intp
-from matplotlib.widgets import Button
+from matplotlib import cbook
+from matplotlib.widgets import AxesWidget, Button, RadioButtons
+
+
+class MyRadioButtons(RadioButtons):
+    def __init__(
+        self,
+        ax,
+        labels,
+        active=0,
+        activecolor="blue",
+        size=49,
+        orientation="vertical",
+        **kwargs,
+    ):
+        """
+        Add radio buttons to an `~.axes.Axes`.
+        Parameters
+        ----------
+        ax : `~matplotlib.axes.Axes`
+            The axes to add the buttons to.
+        labels : list of str
+            The button labels.
+        active : int
+            The index of the initially selected button.
+        activecolor : color
+            The color of the selected button.
+        size : float
+            Size of the radio buttons
+        orientation : str
+            The orientation of the buttons: 'vertical' (default), or 'horizontal'.
+        Further parameters are passed on to `Legend`.
+
+        Notes
+        -----
+        Reference:
+        https://stackoverflow.com/questions/55095111/displaying-radio-buttons-horizontally-in-matplotlib
+
+        """
+        AxesWidget.__init__(self, ax)
+        self.activecolor = activecolor
+        axcolor = ax.get_facecolor()
+        self.value_selected = None
+
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_navigate(False)
+
+        circles = []
+        for i, label in enumerate(labels):
+            if i == active:
+                self.value_selected = label
+                facecolor = activecolor
+            else:
+                facecolor = axcolor
+            p = ax.scatter(
+                [],
+                [],
+                s=size,
+                marker="o",
+                edgecolor="black",
+                facecolor=facecolor,
+            )
+            circles.append(p)
+        if orientation == "horizontal":
+            kwargs.update(ncol=len(labels), mode="expand")
+        kwargs.setdefault("frameon", False)
+        self.box = ax.legend(circles, labels, loc="center", **kwargs)
+        self.labels = self.box.texts
+        self.circles = self.box.legendHandles
+        for c in self.circles:
+            c.set_picker(5)
+
+        self._observers = cbook.CallbackRegistry()
+
+        self.connect_event("pick_event", self._clicked)
+
+    def _clicked(self, event):
+        if (
+            self.ignore(event)
+            or event.mouseevent.button != 1
+            or event.mouseevent.inaxes != self.ax
+        ):
+            return
+        if event.artist in self.circles:
+            self.set_active(self.circles.index(event.artist))
 
 
 def get_attributes(data, key):
@@ -70,8 +155,8 @@ class UpdatePlots:
         self.ax["A"].clear()
         x_pos = self.pos[:, 0]
         y_pos = self.pos[:, 1]
+        print(self._key)
         scalar = self.scalar_fns[self._key](self.pulses, self.pos, self.meta)
-        print(scalar.shape)
         if self.hack:
             for index in range(len(scalar)):
                 if index % 8 == 0:
@@ -121,6 +206,7 @@ class UpdatePlots:
             lw=1.0,
             picker=True,
         )
+        self.fig.canvas.draw_idle()
 
     def onpick(self, event):
         if event.artist != self.antenna_plt:
@@ -165,6 +251,19 @@ class UpdatePlots:
             pass
         self.fig.canvas.draw_idle()
         return True
+
+    def update_skeys(self, skey=None):
+        if skey is not None:
+            self.key = skey
+            print(self.key)
+            self.ax["A"].clear()
+            self.update_plots()
+
+    def update_dkeys(self, dkey=None):
+        if dkey is not None:
+            self.dkey = dkey
+            self.ax["A"].clear()
+            self.update_plots()
 
     def box_plots(self, event):
         self.ax["C"].clear()
@@ -243,22 +342,51 @@ def view(
     bnext.on_clicked(defaultevent.clear_plots)
 
     # Buttons for the scalar plots
-    axscalar = []
-    buttonscalar = []
+    upplt = UpdatePlots(fig, ax, scalar_fns, data)
+    """
     axdata = []
     buttondata = []
+    for i, dkey in enumerate(data.keys()):
+        axdata.append(fig.add_axes([0.06 * (i + 1), 0.07, 0.05, 0.025]))
+        buttondata.append(Button(axdata[i], dkey))
+        upplt = UpdatePlots(
+            fig,
+            ax,
+            scalar_fns,
+            data,
+            data_key=dkey,
+            # scalar_key=upplt.key,  # keep the previous scalar key
+        )
+        buttondata[i].on_clicked(upplt.update_plots)
+        fig.canvas.mpl_connect("pick_event", upplt.onpick)
+
+    axscalar = []
+    buttonscalar = []
     for i, key in enumerate(scalar_fns):
         axscalar.append(fig.add_axes([0.06 * (i + 1), 0.97, 0.05, 0.025]))
         buttonscalar.append(Button(axscalar[i], key))
-        upplt = UpdatePlots(fig, ax, scalar_fns, data, scalar_key=key)
+        upplt.key = key
         buttonscalar[i].on_clicked(upplt.update_plots)
         fig.canvas.mpl_connect("pick_event", upplt.onpick)
-    for j, dkey in enumerate(data.keys()):
-        axdata.append(fig.add_axes([0.06 * (j + 1), 0.07, 0.05, 0.025]))
-        buttondata.append(Button(axdata[j], dkey))
-        upplt = UpdatePlots(fig, ax, scalar_fns, data, data_key=dkey)
-        buttondata[j].on_clicked(upplt.update_plots)
-        fig.canvas.mpl_connect("pick_event", upplt.onpick)
+    """
+    print(scalar_fns.keys())
+    axradio = fig.add_axes([0.06, 0.97, 0.07 * len(scalar_fns.keys()), 0.025])
+    radio = MyRadioButtons(
+        axradio,
+        scalar_fns.keys(),
+        orientation="horizontal",
+        active=0,
+    )
+    radio.on_clicked(upplt.update_skeys)
+    axradio2 = fig.add_axes([0.06, 0.07, 0.05 * len(data.keys()), 0.025])
+    radio2 = MyRadioButtons(
+        axradio2,
+        data.keys(),
+        orientation="horizontal",
+        active=0,
+    )
+    radio2.on_clicked(upplt.update_dkeys)
+    fig.canvas.mpl_connect("pick_event", upplt.onpick)
 
     axclear = fig.add_axes([0.45, 0.01, 0.05, 0.025])
     bbox = Button(axclear, "Box Plots")
